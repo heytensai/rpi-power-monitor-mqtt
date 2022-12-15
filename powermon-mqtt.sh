bdc8ff6042596979f52b6b417723f4b68544b208
Corey <tensai@zmonkey.org>
2022-12-14 20:12:58 -0700

    add some descriptions for the available variables. change CT numbers to match version 0.2.0. closes #6

--- powermon-mqtt.sh
+++ powermon-mqtt.sh
@@ -2,7 +2,11 @@
 
 [ "${FLOCKER}" != "$0" ] && exec env FLOCKER="$0" flock -en "$0" "$0" "$@" || :
 
+# your MQTT broker. if using Home Assistant, this is probably your HA IP address
 HOST="${1}"
+
+# a prefix for the MQTT topic. Default is "powermon", but if you have multiple then
+# you could use "powermon/shop", "powermon/home", etc.
 PREFIX="${2}"
 if [ "${PREFIX}" = "" ]
 then
@@ -10,10 +14,14 @@ then
 fi
 
 # if your MQTT broker requires authentication, or leave blank
+# if using Mosquitto in Home Assistant, the username is "homeassistant"
 MQTT_UN=""
+# if using Mosquitto in Home Assistant this is found in
+# Settings -> Devices & Services -> Mosquitto Broker Configure -> MQTT settings -> Password
 MQTT_PW=""
 
-for ct in $(seq 0 5)
+# versions before 0.2.0 used 0 through 5
+for ct in $(seq 1 6)
 do
 	w=$(influx -database power_monitor -execute "select mean(power) from raw_cts where ct = '${ct}' and time > now() - 1m group by power fill(0) limit 1" -format csv |grep ^raw_cts |perl -lpe 's/.*,//; s/(\..).*/$1/; $_=0 if ($_<0);')
 	mosquitto_pub -u "${MQTT_UN}" -P "${MQTT_PW}" -h "${HOST}" -t "${PREFIX}/ct_${ct}/power" -m "${w}"
